dadf506b8f18ca3e61cac869e835ceb32052b044
General T <102709685+The-General-T@users.noreply.github.com>
2022-09-12 12:08:00 -0400

    Moved to Crazy Code Folder

---
+++ Crazy Code/siezureMode.js
@@ -0,0 +1,5 @@
+function siezureMode() {
+  fill(rgb(randomNumber(0,255),randomNumber(0,255),randomNumber(0,255)));
+  noStroke();
+  ellipse(randomNumber(0,400),randomNumber(0,400),randomNumber(0,200),randomNumber(0,200));
+}
